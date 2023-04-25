4e21ad533607df9c13ca68bb9b3b7a4b8c05c3c1
Cwesij <jeffreyoffei1997@gmail.com>
2023-04-25 08:51:08 +0000

    Routing configured using axios

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
-import Catalog from '../../features/catalog/Catolog';
 import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import Header from './Header';
 import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import AboutPage from '../../features/about/AboutPage';
+import ContactPage from '../../features/contact/ContactPage';
+import Catalog from '../../features/catalog/Catalog';
 
 
 function App() {
@@ -27,7 +32,13 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog/:id" element={<ProductDetails />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
       </Container> 
     </ThemeProvider>
   );
